c69de05ee0e5ac2e126e85705e88c75e5ac04df9
buxiangjie <bxj3416162@163.com>
2020-08-07 17:51:39 +0800

    1

---
+++ testcase/conftest.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+"""
+@auth:buxiangjie
+@date:2020-05-12 11:26:00
+@describe: 
+"""
+
+import allure
+import pytest
+import sys
+import os
+
+# 把当前目录的父目录加到sys.path中
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+
+
+def pytest_addoption(parser):
+	parser.addoption("--env", default="qa", help="script run enviroment")
+	parser.addoption("--types", default="-")
+
+
+@pytest.fixture(scope="session")
+def env(request):
+	return request.config.getoption("--env")
+
+@pytest.fixture(scope="session")
+def types(request):
+	return request.config.getoption("--types")
