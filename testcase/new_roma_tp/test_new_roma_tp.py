827e971b4d350bb75b28c5da55b2abdc19026e87
buxiangjie <bxj3416162@163.com>
2020-08-31 15:27:51 +0800

    new_roma_tp

--- testcase/new_roma_tp/test_new_roma_tp.py
+++ testcase/new_roma_tp/test_new_roma_tp.py
@@ -11,6 +11,8 @@ import os
 import json
 import time
 import sys
+import allure
+import pytest
 
 from common.common_func import Common
 from log.logger import Logger
@@ -22,49 +24,43 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 logger = Logger(logger="test_new_roma_tp").getlog()
 
+@allure.feature("新罗马车贷")
+class TestNewRomaTp:
+	file = Config().get_item('File', 'new_roma_case_file')
+	excel = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + file
 
-class NewRomaTp(unittest.TestCase):
-
-	@classmethod
-	def setUpClass(cls):
-		cls.env = 'qa'
-		cls.r = Common.conn_redis(enviroment=cls.env)
-		file = Config().get_item('File', 'new_roma_case_file')
-		cls.excel = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) + file
-
-	@classmethod
-	def tearDownClass(cls):
-		pass
-
-	def test_100_credit_apply(self):
+	@allure.title("额度授信")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_100_credit_apply(self, r, env):
 		"""额度授信"""
 		data = excel_table_byname(self.excel, 'credit_apply_data_new')
 		print("接口名称:%s" % data[0]['casename'])
-		Common.p2p_get_userinfo('new_roma', self.env)
-		self.r.mset(
+		Common.p2p_get_userinfo('new_roma', env)
+		r.mset(
 			{
 				"new_roma_sourceUserId": Common.get_random('userid'),
 				"new_roma_transactionId": Common.get_random('transactionId'),
 				"new_roma_phone": Common.get_random('phone'),
-				"new_roma_firstCreditDate": Common.get_time(),
+				"new_roma_firstCreditDate": Common.get_time("-"),
 			}
 		)
 		param = json.loads(data[0]['param'])
 		param['personalInfo'].update(
 			{
-				"cardNum": self.r.get('new_roma_cardNum'),
-				"custName": self.r.get('new_roma_custName'),
-				"phone": self.r.get('new_roma_phone')
+				"cardNum": r.get('new_roma_cardNum'),
+				"custName": r.get('new_roma_custName'),
+				"phone": r.get('new_roma_phone')
 			}
 		)
 		param['applyInfo'].update({"applyTime": Common.get_time()})
 		param['riskSuggestion'].update(
-			{"firstCreditDate": self.r.get('new_roma_firstCreditDate')})
+			{"firstCreditDate": r.get('new_roma_firstCreditDate')})
 		param.update(
 			{
-				"sourceUserId": self.r.get('new_roma_sourceUserId'),
+				"sourceUserId": r.get('new_roma_sourceUserId'),
 				"serviceSn": Common.get_random('serviceSn'),
-				"transactionId": self.r.get('new_roma_transactionId')
+				"transactionId": r.get('new_roma_transactionId')
 			}
 		)
 		param['orderDetail']['employeeNum'] = 10
@@ -84,26 +80,27 @@ class NewRomaTp(unittest.TestCase):
 			faceaddr=data[0]['url'], headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		creditId = rep['content']['creditId']
-		userId = rep['content']['userId']
-		self.r.mset(
+		r.mset(
 			{
-				"new_roma_creditId": creditId,
-				"new_roma_userId": userId
+				"new_roma_creditId": rep['content']['creditId'],
+				"new_roma_userId": rep['content']['userId']
 			}
 		)
-		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
+		assert (rep['resultCode'], int(data[0]['resultCode']))
 
-	def test_101_upload_image(self):
+	@allure.title("图片上传")
+	@allure.severity("normal")
+	@pytest.mark.asset
+	def test_101_upload_image(self, r, env):
 		"""图片上传：授信"""
 		data = excel_table_byname(self.excel, 'image_upload')
 		print("接口名称:%s" % data[0]['casename'])
 		param = json.loads(data[0]['param'])
 		param.update(
 			{
-				"associationId": self.r.get('new_roma_creditId'),
+				"associationId": r.get('new_roma_creditId'),
 				"bizType": 1
 			}
 		)
@@ -116,11 +113,14 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
+		assert (rep['resultCode'], int(data[0]['resultCode']))
 
-	def test_1011_sign_credit(self):
+	@allure.title("上传授信协议")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_1011_sign_credit(self, r, env):
 		"""上传授信协议"""
 		data = excel_table_byname(self.excel, 'contract_sign')
 		print("接口名称:%s" % data[0]['casename'])
@@ -128,11 +128,11 @@ class NewRomaTp(unittest.TestCase):
 		param.update(
 			{
 				"serviceSn": Common.get_random('serviceSn'),
-				"sourceUserId": self.r.get('new_roma_sourceUserId'),
+				"sourceUserId": r.get('new_roma_sourceUserId'),
 				"contractType": 1,
 				"sourceContractId": Common.get_random('userid'),
-				"transactionId": self.r.get('new_roma_transactionId'),
-				"associationId": self.r.get('new_roma_creditId')
+				"transactionId": r.get('new_roma_transactionId'),
+				"associationId": r.get('new_roma_creditId')
 			}
 		)
 		if len(data[0]['headers']) == 0:
@@ -144,18 +144,20 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
+		assert (rep['resultCode'], int(data[0]['resultCode']))
 
-	def test_102_query_result(self):
+	@allure.title("授信结果查询")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_102_query_result(self, r, env):
 		"""授信结果查询"""
-		time.sleep(3)
-		GetSqlData.credit_set(enviroment=self.env, credit_id=self.r.get("new_roma_creditId"))
+		GetSqlData.credit_set(enviroment=env, credit_id=r.get("new_roma_creditId"))
 		data = excel_table_byname(self.excel, 'credit_query_result')
 		print("接口名称:%s" % data[0]['casename'])
 		param = json.loads(data[0]['param'])
-		param.update({"creditId": self.r.get('new_roma_creditId')})
+		param.update({"creditId": r.get('new_roma_creditId')})
 		if len(data[0]['headers']) == 0:
 			headers = None
 		else:
@@ -165,19 +167,22 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
+		assert (rep['resultCode'], int(data[0]['resultCode']))
 
-	def test_103_query_user_amount(self):
+	@allure.title("用户额度查询")
+	@allure.severity("normal")
+	@pytest.mark.asset
+	def test_103_query_user_amount(self, r, env):
 		"""用户额度查询"""
 		data = excel_table_byname(self.excel, 'query_user_amount')
 		print("接口名称:%s" % data[0]['casename'])
 		param = json.loads(data[0]['param'])
 		param.update(
 			{
-				"sourceUserId": self.r.get('new_roma_sourceUserId'),
-				"userId": self.r.get('new_roma_userId'),
+				"sourceUserId": r.get('new_roma_sourceUserId'),
+				"userId": r.get('new_roma_userId'),
 				# "productCode": "XJ_ROMA_CAR"
 			}
 		)
@@ -190,32 +195,35 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
+		assert (rep['resultCode'], int(data[0]['resultCode']))
 		GetSqlData.check_user_amount(
-			user_id=self.r.get('new_roma_userId'),
-			enviroment=self.env
+			user_id=r.get('new_roma_userId'),
+			enviroment=env
 		)
 
-	def test_104_project_apply(self):
+	@allure.title("进件申请")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_104_project_apply(self, r, env):
 		"""进件"""
 		data = excel_table_byname(self.excel, 'test_project')
 		print("接口名称:%s" % data[0]['casename'])
 		param = json.loads(data[0]['param'])
-		self.r.set('new_roma_sourceProjectId', Common.get_random('sourceProjectId'))
+		r.set('new_roma_sourceProjectId', Common.get_random('sourceProjectId'))
 		param.update(
 			{
-				"sourceProjectId": self.r.get('new_roma_sourceProjectId'),
-				"sourceUserId": self.r.get('new_roma_sourceUserId'),
-				"transactionId": self.r.get('new_roma_transactionId')
+				"sourceProjectId": r.get('new_roma_sourceProjectId'),
+				"sourceUserId": r.get('new_roma_sourceUserId'),
+				"transactionId": r.get('new_roma_transactionId')
 			}
 		)
 		param['personalInfo'].update(
 			{
-				"cardNum": self.r.get('new_roma_cardNum'),
-				"custName": self.r.get('new_roma_custName'),
-				"phone": self.r.get('new_roma_phone'),
+				"cardNum": r.get('new_roma_cardNum'),
+				"custName": r.get('new_roma_custName'),
+				"phone": r.get('new_roma_phone'),
 				"hasChildren": "Y"
 			}
 		)
@@ -226,7 +234,7 @@ class NewRomaTp(unittest.TestCase):
 			}
 		)
 		param['riskSuggestion'].update(
-			{"firstCreditDate": self.r.get('new_roma_firstCreditDate')})
+			{"firstCreditDate": r.get('new_roma_firstCreditDate')})
 		param['cardInfo'].update(
 			{
 				"bankNameSub": "建设银行",
@@ -244,23 +252,27 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.r.set('new_roma_projectId', rep['content']['projectId'])
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
-		GetSqlData.change_project_audit_status(
-			project_id=self.r.get('new_roma_projectId'),
-			enviroment=self.env)
+		r.set('new_roma_projectId', rep['content']['projectId'])
+		assert (int(data[0]['resultCode']), rep['resultCode'])
 
-	def test_105_query_apply_result(self):
+	@allure.title("进件结果查询")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_105_query_apply_result(self, r, env):
 		"""进件结果查询"""
 		data = excel_table_byname(self.excel, 'project_query_apply_result')
 		print("接口名称:%s" % data[0]['casename'])
+		GetSqlData.change_project_audit_status(
+			project_id=r.get('new_roma_projectId'),
+			enviroment=env
+		)
 		param = json.loads(data[0]['param'])
 		param.update(
 			{
-				"sourceProjectId": self.r.get('new_roma_sourceProjectId'),
-				"projectId": self.r.get('new_roma_projectId')
+				"sourceProjectId": r.get('new_roma_sourceProjectId'),
+				"projectId": r.get('new_roma_projectId')
 			}
 		)
 		if len(data[0]['headers']) == 0:
@@ -272,12 +284,15 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
-		self.assertEqual(rep['content']['auditStatus'], 2)
+		assert (int(data[0]['resultCode']), rep['resultCode'])
+		assert (rep['content']['auditStatus'], 2)
 
-	def test_106_contract_sign(self):
+	@allure.title("上传借款合同")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_106_contract_sign(self, r, env):
 		"""上传借款合同"""
 		data = excel_table_byname(self.excel, 'contract_sign')
 		print("接口名称:%s" % data[0]['casename'])
@@ -285,11 +300,11 @@ class NewRomaTp(unittest.TestCase):
 		param.update(
 			{
 				"serviceSn": Common.get_random('serviceSn'),
-				"sourceUserId": self.r.get('new_roma_sourceUserId'),
+				"sourceUserId": r.get('new_roma_sourceUserId'),
 				"contractType": 2,
 				"sourceContractId": Common.get_random('userid'),
-				"transactionId": self.r.get('new_roma_transactionId'),
-				"associationId": self.r.get('new_roma_projectId')
+				"transactionId": r.get('new_roma_transactionId'),
+				"associationId": r.get('new_roma_projectId')
 			}
 		)
 		if len(data[0]['headers']) == 0:
@@ -301,11 +316,14 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
+		assert (int(data[0]['resultCode']), rep['resultCode'])
 
-	def test_107_pfa(self):
+	@allure.title("申请放款")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_107_pfa(self, r, env):
 		"""放款"""
 		time.sleep(5)
 		data = excel_table_byname(self.excel, 'project_loan')
@@ -313,17 +331,17 @@ class NewRomaTp(unittest.TestCase):
 		param = json.loads(data[0]['param'])
 		param.update(
 			{
-				"sourceProjectId": self.r.get('new_roma_sourceProjectId'),
-				"projectId": self.r.get('new_roma_projectId'),
-				"sourceUserId": self.r.get('new_roma_sourceUserId'),
+				"sourceProjectId": r.get('new_roma_sourceProjectId'),
+				"projectId": r.get('new_roma_projectId'),
+				"sourceUserId": r.get('new_roma_sourceUserId'),
 				"serviceSn": Common.get_random('serviceSn'),
-				"accountName": self.r.get('new_roma_custName'),
-				"id": self.r.get('new_roma_cardNum'),
+				"accountName": r.get('new_roma_custName'),
+				"id": r.get('new_roma_cardNum'),
 				"bankCode": "CCB",
 				"accountNo": "6227002432220410613"  # 6227003814170172872
 			}
 		)
-		self.r.set("new_roma_pfa_serviceSn", param['serviceSn'])
+		r.set("new_roma_pfa_serviceSn", param['serviceSn'])
 		if len(data[0]['headers']) == 0:
 			headers = None
 		else:
@@ -333,25 +351,28 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
+		assert (int(data[0]['resultCode']), rep['resultCode'])
 		time.sleep(8)
 		# 修改支付表中的品钛返回code
 		GetSqlData.change_pay_status(
-			enviroment=self.env,
-			project_id=self.r.get('new_roma_projectId')
+			enviroment=env,
+			project_id=r.get('new_roma_projectId')
 		)
 
-	def test_108_query(self):
+	@allure.title("放款结果查询")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_108_query(self, r, env):
 		"""放款结果查询"""
-		GetSqlData.loan_set(enviroment=self.env, project_id=self.r.get('new_roma_projectId'))
+		GetSqlData.loan_set(enviroment=env, project_id=r.get('new_roma_projectId'))
 		data = excel_table_byname(self.excel, 'query')
 		print("接口名称:%s" % data[0]['casename'])
 		param = json.loads(data[0]['param'])
 		param.update(
 			{
-				"serviceSn": self.r.get("new_roma_pfa_serviceSn")
+				"serviceSn": r.get("new_roma_pfa_serviceSn")
 			}
 		)
 		if len(data[0]['headers']) == 0:
@@ -363,18 +384,21 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
+		assert (int(data[0]['resultCode']), rep['resultCode'])
 
-	def test_109_query_repayment_plan(self):
+	@allure.title("还款计划查询")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_109_query_repayment_plan(self, r, env):
 		"""还款计划查询"""
 		data = excel_table_byname(self.excel, 'query_repayment_plan')
 		param = json.loads(data[0]['param'])
 		param.update(
 			{
 				"transactionId": Common.get_random("transactionId"),
-				"projectId": self.r.get('new_roma_projectId')
+				"projectId": r.get('new_roma_projectId')
 			}
 		)
 		if len(data[0]['headers']) == 0:
@@ -386,18 +410,21 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
+		assert (int(data[0]['resultCode']), rep['resultCode'])
 
-	def test_110_pre_clear_calculate(self):
+	@allure.title("还款计划试算")
+	@allure.severity("blocker")
+	@pytest.mark.asset
+	def test_110_pre_clear_calculate(self, r, env):
 		"""还款计划试算"""
 		data = excel_table_byname(self.excel, 'calculate')
 		param = json.loads(data[0]['param'])
 		param.update(
 			{
-				"sourceProjectId": self.r.get("new_roma_sourceProjectId"),
-				"projectId": self.r.get('new_roma_projectId')
+				"sourceProjectId": r.get("new_roma_sourceProjectId"),
+				"projectId": r.get('new_roma_projectId')
 			}
 		)
 		if len(data[0]['headers']) == 0:
@@ -409,13 +436,12 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
+		assert (int(data[0]['resultCode']), rep['resultCode'])
 
-	# @unittest.skipUnless(sys.argv[4] == "offline_partial", "-")
-	@unittest.skip("跳过")
-	def test_111_offline_partial(self):
+	@pytest.mark.skip
+	def test_111_offline_partial(self, r, env):
 		"""线下还款:部分还款"""
 		data = excel_table_byname(self.excel, 'offline_partial')
 		print("接口名称:%s" % data[0]['casename'])
@@ -424,9 +450,9 @@ class NewRomaTp(unittest.TestCase):
 			{
 				"sourceRequestId": Common.get_random("requestNum"),
 				"sourceRepaymentId": Common.get_random("requestNum"),
-				"projectId": self.r.get("new_roma_projectId"),
-				"sourceProjectId": self.r.get("new_roma_sourceProjectId"),
-				"sourceUserId": self.r.get("new_roma_sourceUserId"),
+				"projectId": r.get("new_roma_projectId"),
+				"sourceProjectId": r.get("new_roma_sourceProjectId"),
+				"sourceUserId": r.get("new_roma_sourceUserId"),
 				"serviceSn": Common.get_random("serviceSn"),
 				"payTime": Common.get_time("-"),
 				"successAmount": 76,
@@ -442,12 +468,12 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
+		assert (rep['resultCode'], int(data[0]['resultCode']))
 
-	@unittest.skip("-")
-	def test_112_push_reconciliation_result(self):
+	@pytest.mark.skip
+	def test_112_push_reconciliation_result(self, r, env):
 		"""对账结果推送"""
 		data = excel_table_byname(self.excel, 'push_reconciliation_result')
 		param = json.loads(data[0]['param'])
@@ -456,8 +482,8 @@ class NewRomaTp(unittest.TestCase):
 				"accountId": Common.get_random("transactionId")
 			}
 		)
-		for i in range(len(param['resultList'])):
-			param['resultList'][i]['businessDate'] = Common.get_time('day')
+		for i in param['resultList']:
+			i['businessDate'] = Common.get_time('day')
 		if len(data[0]['headers']) == 0:
 			headers = None
 		else:
@@ -467,10 +493,10 @@ class NewRomaTp(unittest.TestCase):
 			headers=headers,
 			data=json.dumps(param, ensure_ascii=False),
 			product="cloudloan",
-			enviroment=self.env
+			enviroment=env
 		)
-		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
+		assert (int(data[0]['resultCode']), rep['resultCode'])
 
 
 if __name__ == '__main__':
-	unittest.main()
+	pytest.main()
