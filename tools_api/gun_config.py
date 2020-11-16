32d275d658010b03b79989a8a59cb371ac7c0c28
buxiangjie <bxj3416162@163.com>
2020-11-16 15:33:48 +0800

    增加fastapi

---
+++ tools_api/gun_config.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+"""
+@auth:buxiangjie
+@date:2020-05-12 11:26:00
+@describe: 
+"""
+bind = ["192.168.1.119:8817"]
+workers = 2
+daemon = True
+reload = True
+worker_class = "uvicorn.workers.UvicornH11Worker"
+reload_engine = "auto"
+preload_app = True
