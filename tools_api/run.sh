f7372ee3922e0dc7ea24f7da8bbc8ba5f50440d0
buxiangjie <bxj3416162@163.com>
2021-01-12 16:54:08 +0800

    test

--- tools_api/run.sh
+++ tools_api/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-spawn su root
-expect "Password:"
-send "111111\r"
-send "cd /tools_api\r"
-send "python3 start_init.py\r"
-send "gunicorn -c gun_confg.py main:app\r"
-
-expect eof
-exit
+sudo s
+python3 start_init.py
+gunicorn -c gun_config.py main:app
