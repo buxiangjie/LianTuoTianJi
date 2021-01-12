731220dfc45003b9cbd63abd296226934144a4bb
buxiangjie <bxj3416162@163.com>
2021-01-12 11:39:50 +0800

    11

--- tools_api/run.sh
+++ tools_api/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-spawn sudo su
+spawn su root
 expect "Password:"
-send "cd /tools_api\r"
 send "111111\r"
+send "cd /tools_api\r"
 send "python3 start_init.py\r"
 send "/home/buxj/.local/bin/uvicorn main:app\r"
 
