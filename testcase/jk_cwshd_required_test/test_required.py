8226b39293e955444f8f5fb916f8575aa99287b4
buxiangjie <bxj3416162@163.com>
2021-04-08 13:34:36 +0800

    更新

--- testcase/jk_cwshd_required_test/test_required.py
+++ testcase/jk_cwshd_required_test/test_required.py
@@ -78,149 +78,154 @@ class CreditNone(unittest.TestCase):
 		self.assertEqual(rep['resultCode'], int(data['resultCode']))
 
 
-# @ddt.ddt
-# class ApplyNone(unittest.TestCase):
-# 	file = Config().get_item('File', 'jk_cwshd_required_case_file')
-# 	excel_data = excel_table_byname(file, 'apply_none')
-#
-# 	@classmethod
-# 	def setUpClass(cls):
-# 		cls.env = 'test'
-# 		cls.url = ApplyNone.excel_data[0]['url']
-# 		cls.headers = ApplyNone.excel_data[0]['headers']
-# 		cls.param = ApplyNone.excel_data[0]['param']
-# 		cls.r = Common.conn_redis(environment=cls.env)
-#
-# 	def tearDown(self):
-# 		pass
-#
-# 	def credit(self):
-# 		print("授信------")
-# 		data = excel_table_byname(Config().get_item('File', 'jk_cwshd_case_file'), 'credit')
-# 		Common.p2p_get_userinfo('jk_cwshd_6_periods', self.env)
-# 		self.r.mset(
-# 			{
-# 				"jk_cwshd_6_periods_sourceUserId": Common.get_random('userid'),
-# 				'jk_cwshd_6_periods_transactionId': Common.get_random('transactionId'),
-# 				"jk_cwshd_6_periods_phone": Common.get_random('phone'),
-# 				"jk_cwshd_6_periods_firstCreditDate": Common.get_time()
-# 			}
-# 		)
-# 		param = json.loads(data[0]['param'])
-# 		param['personalInfo'].update(
-# 			{
-# 				"cardNum": self.r.get('jk_cwshd_6_periods_cardNum'),
-# 				"custName": self.r.get('jk_cwshd_6_periods_custName'),
-# 				"phone": self.r.get('jk_cwshd_6_periods_phone')
-# 			}
-# 		)
-# 		param['applyInfo'].update({"applyTime": Common.get_time()})
-# 		param['entityInfo']['unifiedSocialCreditCode'] = Common.get_random("businessLicenseNo")
-# 		param.update(
-# 			{
-# 				"sourceUserId": self.r.get('jk_cwshd_6_periods_sourceUserId'),
-# 				"serviceSn": Common.get_random('serviceSn'),
-# 				"transactionId": self.r.get('jk_cwshd_6_periods_transactionId')
-# 			}
-# 		)
-# 		if len(data[0]['headers']) == 0:
-# 			headers = None
-# 		else:
-# 			headers = json.loads(data[0]['headers'])
-# 		rep = Common.response(
-# 			faceaddr=data[0]['url'],
-# 			headers=headers,
-# 			data=json.dumps(param, ensure_ascii=False),
-# 			product="cloudloan",
-# 			environment=self.env
-# 		)
-# 		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
-# 		self.r.mset(
-# 			{
-# 				"jk_cwshd_6_periods_creditId": rep['content']['creditId'],
-# 				"jk_cwshd_6_periods_userId": rep['content']['userId']
-# 			}
-# 		)
-#
-# 	def query_result(self):
-# 		print("授信结果检查-------")
-# 		GetSqlData.credit_set(
-# 			environment=self.env,
-# 			credit_id=self.r.get("jk_cwshd_6_periods_creditId")
-# 		)
-# 		GetSqlData.check_user_amount(user_id=self.r.get("jk_cwshd_6_periods_userId"), environment=self.env)
-# 		data = excel_table_byname(Config().get_item('File', 'jk_cwshd_case_file'), 'query_result')
-# 		param = json.loads(data[0]['param'])
-# 		param.update({"creditId": self.r.get('jk_cwshd_6_periods_creditId')})
-# 		if len(data[0]['headers']) == 0:
-# 			headers = None
-# 		else:
-# 			headers = json.loads(data[0]['headers'])
-# 		rep = Common.response(
-# 			faceaddr=data[0]['url'],
-# 			headers=headers,
-# 			data=json.dumps(param, ensure_ascii=False),
-# 			product="cloudloan",
-# 			environment=self.env
-# 		)
-# 		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
-# 		self.assertEqual(rep['content']['creditStatus'], 1)
-#
-# 	@ddt.data(*excel_data)
-# 	def test_apply(self, data):
-# 		self.credit()
-# 		self.query_result()
-# 		case = str(data['casename'])
-# 		print(case)
-# 		param = json.loads(self.param)
-# 		param.update(
-# 			{
-# 				"sourceProjectId": Common.get_random("sourceProjectId"),
-# 				"sourceUserId": self.r.get('jk_cwshd_6_periods_sourceUserId'),
-# 				"transactionId": self.r.get('jk_cwshd_6_periods_transactionId')
-# 			}
-# 		)
-# 		param['personalInfo'].update(
-# 			{
-# 				"cardNum": self.r.get('jk_cwshd_6_periods_cardNum'),
-# 				"custName": self.r.get('jk_cwshd_6_periods_custName'),
-# 				"phone": self.r.get('jk_cwshd_6_periods_phone')
-# 			}
-# 		)
-# 		param["cardInfo"]["unifiedSocialCreditCode"] = Common.get_random("businessLicenseNo")
-# 		print(f"""前置条件:{data["前置条件"]}""")
-# 		if len(data["前置条件"]) > 0:
-# 			preconditions = data["前置条件"].split(",")
-# 			for k in preconditions:
-# 				key = k.split(".")[0]
-# 				value = k.split(".")[1].split("==")[0]
-# 				enum = k.split(".")[1].split("==")[1]
-# 				if Common.is_number(enum):
-# 					enum = int(enum)
-# 				param[key][value] = enum
-# 		if "==" in case:
-# 			_key = case.split("围")[1].split(".")[0]
-# 			_value = case.split("围")[1].split(".")[1].split("==")[0]
-# 			_enum = case.split("围")[1].split(".")[1].split("==")[1].replace(" ", "")
-# 			if Common.is_number(_enum):
-# 				_enum = int(_enum)
-# 			param[_key][_value] = _enum
-# 		elif "." in case:
-# 			_key = case.split("空")[1].split(".")[0]
-# 			_value = case.split("空")[1].split(".")[1]
-# 			if _key == _value:
-# 				param[_key] = None
-# 			else:
-# 				param[_key][_value] = None
-# 		headers = json.loads(self.headers)
-# 		rep = Common.response(
-# 			faceaddr=self.url,
-# 			headers=headers,
-# 			data=json.dumps(param, ensure_ascii=False),
-# 			product='cloudloan',
-# 			environment=self.env
-# 		)
-# 		self.assertEqual(rep['resultCode'], int(data['resultCode']))
+@ddt.ddt
+class ApplyNone(unittest.TestCase):
+	file = Config().get_item('File', 'jk_cwshd_required_case_file')
+	excel_data = excel_table_byname(file, 'apply_none')
+
+	@classmethod
+	def setUpClass(cls):
+		cls.env = 'test'
+		cls.url = ApplyNone.excel_data[0]['url']
+		cls.headers = ApplyNone.excel_data[0]['headers']
+		cls.param = ApplyNone.excel_data[0]['param']
+		cls.r = Common.conn_redis(environment=cls.env)
+
+	def tearDown(self):
+		pass
+
+	def credit(self):
+		print("授信------")
+		data = excel_table_byname(Config().get_item('File', 'jk_cwshd_case_file'), 'credit')
+		Common.p2p_get_userinfo('jk_cwshd_6_periods', self.env)
+		self.r.mset(
+			{
+				"jk_cwshd_6_periods_sourceUserId": Common.get_random('userid'),
+				'jk_cwshd_6_periods_transactionId': Common.get_random('transactionId'),
+				"jk_cwshd_6_periods_phone": Common.get_random('phone'),
+				"jk_cwshd_6_periods_firstCreditDate": Common.get_time()
+			}
+		)
+		param = json.loads(data[0]['param'])
+		param['personalInfo'].update(
+			{
+				"cardNum": self.r.get('jk_cwshd_6_periods_cardNum'),
+				"custName": self.r.get('jk_cwshd_6_periods_custName'),
+				"phone": self.r.get('jk_cwshd_6_periods_phone')
+			}
+		)
+		param['applyInfo'].update({"applyTime": Common.get_time()})
+		param['entityInfo']['unifiedSocialCreditCode'] = Common.get_random("businessLicenseNo")
+		param.update(
+			{
+				"sourceUserId": self.r.get('jk_cwshd_6_periods_sourceUserId'),
+				"serviceSn": Common.get_random('serviceSn'),
+				"transactionId": self.r.get('jk_cwshd_6_periods_transactionId')
+			}
+		)
+		if len(data[0]['headers']) == 0:
+			headers = None
+		else:
+			headers = json.loads(data[0]['headers'])
+		rep = Common.response(
+			faceaddr=data[0]['url'],
+			headers=headers,
+			data=json.dumps(param, ensure_ascii=False),
+			product="cloudloan",
+			environment=self.env
+		)
+		self.assertEqual(rep['resultCode'], int(data[0]['resultCode']))
+		self.r.mset(
+			{
+				"jk_cwshd_6_periods_creditId": rep['content']['creditId'],
+				"jk_cwshd_6_periods_userId": rep['content']['userId']
+			}
+		)
+
+	def query_result(self):
+		print("授信结果检查-------")
+		GetSqlData.credit_set(
+			environment=self.env,
+			credit_id=self.r.get("jk_cwshd_6_periods_creditId")
+		)
+		GetSqlData.check_user_amount(user_id=self.r.get("jk_cwshd_6_periods_userId"), environment=self.env)
+		data = excel_table_byname(Config().get_item('File', 'jk_cwshd_case_file'), 'query_result')
+		param = json.loads(data[0]['param'])
+		param.update({"creditId": self.r.get('jk_cwshd_6_periods_creditId')})
+		if len(data[0]['headers']) == 0:
+			headers = None
+		else:
+			headers = json.loads(data[0]['headers'])
+		rep = Common.response(
+			faceaddr=data[0]['url'],
+			headers=headers,
+			data=json.dumps(param, ensure_ascii=False),
+			product="cloudloan",
+			environment=self.env
+		)
+		self.assertEqual(int(data[0]['resultCode']), rep['resultCode'])
+		self.assertEqual(rep['content']['creditStatus'], 1)
+
+	@ddt.data(*excel_data)
+	def test_apply(self, data):
+		self.credit()
+		self.query_result()
+		case = str(data['casename'])
+		print(case)
+		param = json.loads(self.param)
+		param.update(
+			{
+				"sourceProjectId": Common.get_random("sourceProjectId"),
+				"sourceUserId": self.r.get('jk_cwshd_6_periods_sourceUserId'),
+				"transactionId": self.r.get('jk_cwshd_6_periods_transactionId')
+			}
+		)
+		param['personalInfo'].update(
+			{
+				"cardNum": self.r.get('jk_cwshd_6_periods_cardNum'),
+				"custName": self.r.get('jk_cwshd_6_periods_custName'),
+				"phone": self.r.get('jk_cwshd_6_periods_phone')
+			}
+		)
+		param["applyInfo"]["applyAmount"] = 30000
+		param["loanInfo"]["loanAmount"] = 30000
+		param["cardInfo"]["unifiedSocialCreditCode"] = Common.get_random("businessLicenseNo")
+		print(f"""前置条件:{data["前置条件"]}""")
+		if len(data["前置条件"]) > 0:
+			preconditions = data["前置条件"].split(",")
+			for k in preconditions:
+				key = k.split(".")[0]
+				value = k.split(".")[1].split("==")[0]
+				enum = k.split(".")[1].split("==")[1]
+				if Common.is_number(enum):
+					enum = int(enum)
+				param[key][value] = enum
+		if "==" in case:
+			_key = case.split("围")[1].split(".")[0]
+			_value = case.split("围")[1].split(".")[1].split("==")[0]
+			_enum = case.split("==")[1].replace(" ", "")
+			if Common.is_number(_enum):
+				if _enum.isdigit():
+					_enum = int(_enum)
+				else:
+					_enum = float(_enum)
+			param[_key][_value] = _enum
+		elif "." in case:
+			_key = case.split("空")[1].split(".")[0]
+			_value = case.split("空")[1].split(".")[1]
+			if _key == _value:
+				param[_key] = None
+			else:
+				param[_key][_value] = None
+		headers = json.loads(self.headers)
+		rep = Common.response(
+			faceaddr=self.url,
+			headers=headers,
+			data=json.dumps(param, ensure_ascii=False),
+			product='cloudloan',
+			environment=self.env
+		)
+		self.assertEqual(rep['resultCode'], int(data['resultCode']))
 
 
 if __name__ == '__main__':
